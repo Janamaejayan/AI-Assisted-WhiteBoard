6ca2e4de787fae06180ab5ad89b1a59907319894
Jana <janamaejayan996@gmail.com>
2026-09-05 10:52:36 +0530

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>This is Janamaejayan V S. I am gonna start building Projects and Understand DSA and System Design(HLD and LLD). I am gonna get placed in any top quant company after 2 years of experienece in HSBC.</h1>
+    </div>
+  )
+};
